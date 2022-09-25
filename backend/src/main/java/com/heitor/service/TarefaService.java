df13290d1c5adc8283963ecf5e761c1c5718a7b9
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-25 20:28:14 -0300

    Criando services

---
+++ backend/src/main/java/com/heitor/service/TarefaService.java
@@ -0,0 +1,28 @@
+package com.heitor.service;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heitor.domain.Tarefa;
+import com.heitor.repository.TarefaRepository;
+
+@Service
+public class TarefaService {
+
+	private final TarefaRepository repo;
+	
+	@Autowired
+	public TarefaService(TarefaRepository repo) {
+		this.repo = repo;
+	}
+	
+	public Tarefa save(Tarefa tarefa) {
+		return repo.save(tarefa);
+	}
+	
+	public Tarefa findById(Integer id) {
+		Tarefa tarefa = repo.findById(id).orElseThrow(()-> new ObjectNotFoundException("Tarefa não encontrada: ", TarefaService.class.getName()));
+		return tarefa;
+	}
+}
