f3eee0bb070395dbd3aabe186251d3eb9805e73b
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-26 08:54:53 -0300

    Criando model Tarefa

---
+++ frontend/src/app/model/Tarefa.ts
@@ -0,0 +1,7 @@
+export class Tarefa{
+    id              : number  | undefined;
+    descricao       : String  | undefined;
+    realizado       : Boolean | undefined;
+    tarefaCriada    : String  | undefined;
+    tarefaConcluida : String  | undefined;
+}
