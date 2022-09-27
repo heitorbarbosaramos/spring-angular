db14b5e67f973595e27b96c36fda6ef44ac526d9
Heitor Fernando Barbosa Ramos <heitorhfbr@gmail.com>
2022-09-27 07:59:30 -0300

    Validando formulario

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms'
+import { FormGroup, FormBuilder ,FormControl, Validators } from '@angular/forms'
 import { TarefaService } from 'src/service/tarefa.service';
 import { Tarefa } from './model/Tarefa';
 
@@ -10,46 +10,54 @@ import { Tarefa } from './model/Tarefa';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private service: TarefaService) { }
+  constructor(private service: TarefaService, private formBuilder: FormBuilder) { }
 
 
   listTarefa: Tarefa[] = [];
-
+  formulario!: FormGroup;
+  
   ngOnInit(): void {
+
     this.listarTarefas();
+    this.validaFormulario();
+       
+  }
+
+  //https://www.alura.com.br/artigos/como-aplicar-validacao-formularios-reativos-angular
+  validaFormulario(){
+    this.formulario = this.formBuilder.group({
+      descricao: new FormControl('', [Validators.required, Validators.minLength(5)])
+    })
   }
 
   listarTarefas() {
     return this.service.listar().subscribe(t => this.listTarefa = t);
   }
 
-  form: FormGroup = new FormGroup({
-    descricao: new FormControl('')
-  })
 
   salvar() {
-    console.log(this.form.value)
-    const tarefa: Tarefa = { ...this.form.value };
+    console.log(this.formulario.value)
+    const tarefa: Tarefa = { ...this.formulario.value };
+    console.log(tarefa)
     this.service.salvar(tarefa).subscribe(t => {
       this.listTarefa.push(tarefa);
-      this.form.reset()
-    }
-    );
+      this.formulario.reset();
+    })
   }
 
-  finalizarTarefa(tarefa: Tarefa) {
-    tarefa.realizado = !tarefa.realizado;
-    this.service.update(tarefa).subscribe(
-      t => {
-        console.log("ATUALIZADO ", t);
-        this.listarTarefas();
-      })
+  finalizarTarefa(tarefa : Tarefa){
+    tarefa.realizado =!tarefa.realizado
+    this.service.update(tarefa).subscribe(t=>{
+      console.log("ATUALIZADO ", t)
+      this.listarTarefas();
+    })
   }
 
-  delete(id: number) {
+  delete(id: number){
     this.service.delete(id).subscribe({
-      next: (response) => this.listarTarefas()
-    }
-    )
+      next:(response) => this.listarTarefas()
+    })
   }
+ 
+
 }
